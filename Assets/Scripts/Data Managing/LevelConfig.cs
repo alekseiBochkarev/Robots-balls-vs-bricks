f9b1f526ad302dc57f474a15ea2dd63651380359
Aleksei Bochkarev <aleksei.bochkarev@brandmaker.com>
2022-09-26 10:20:56 +0700

    add autosize to levelConfig

--- Assets/Scripts/Data Managing/LevelConfig.cs
+++ Assets/Scripts/Data Managing/LevelConfig.cs
@@ -6,18 +6,20 @@ using CodeMonkey.Utils;
 public class LevelConfig : MonoBehaviour
 {
     [SerializeField] private int gridWidth;
-    [SerializeField] private int gridHeight;
-    [SerializeField] private float cellSize;
+    private int gridHeight;
+    private float cellSize;
+    [SerializeField] float baseCellSize;
     [SerializeField] private Vector3 originPosition;
     public Grid grid;
     public GameObject gridColliderPrefab;
      
     void Awake()
     {
+        gridHeight = gridWidth * 4 / 3;
         grid = new Grid(gridWidth, gridHeight, cellSize, originPosition);
         //grid.SetValue(2,2,2);
         //grid.SetValue(1,1,1);
-
+        cellSize = baseCellSize * 6 / gridWidth;
        /* for (int y = 0; y < grid.GridHeight; y++) {
             for (int x = 0; x < grid.GridWidth; x ++) {
                 GameObject gridObj = Instantiate(gridColliderPrefab, grid.GetWorldPosition(x, y), new Quaternion(0, 180, 0, 1));
