1d3a5a5cf37d8fd15c5243bdb2c9c6c8951c7c40
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2023-05-21 16:58:30 +0700

    add sceneconfiguration5

---
+++ Assets/Scripts/Placing/SceneConfiguration5.cs
@@ -0,0 +1,60 @@
+﻿public class SceneConfiguration5 : SceneConfiguration
+{
+    void Awake()
+    {
+        _objectGamePositions = new[]
+        {
+            new ObjectGamePosition("enemies/Brick3", 0, 1, 10), 
+            new ObjectGamePosition("enemies/Brick3", 1, 1, 10),
+            new ObjectGamePosition("enemies/Brick3", 2, 1, 10), 
+            new ObjectGamePosition("enemies/Brick3", 5, 1, 10),
+            new ObjectGamePosition("enemies/Brick3", 6, 1, 10), 
+            new ObjectGamePosition("enemies/Brick3", 9, 1, 10),
+            new ObjectGamePosition("enemies/Brick3", 10, 1, 10), 
+            new ObjectGamePosition("enemies/Brick3", 11, 1, 10),
+            
+            new ObjectGamePosition("enemies/Brick3", 0, 2, 10), 
+            new ObjectGamePosition("enemies/Brick3", 1, 2, 10),
+            new ObjectGamePosition("enemies/Brick3", 4, 2, 10), 
+            new ObjectGamePosition("enemies/Brick3", 7, 2, 10),
+            new ObjectGamePosition("enemies/Brick3", 10, 2, 10), 
+            new ObjectGamePosition("enemies/Brick3", 11, 2, 10),
+
+            new ObjectGamePosition("enemies/Brick2", 0, 3, 20), 
+            new ObjectGamePosition("enemies/Brick2", 3, 3, 20),
+            new ObjectGamePosition("enemies/Brick2", 4, 3, 20), 
+            new ObjectGamePosition("enemies/Brick2", 7, 3, 20),
+            new ObjectGamePosition("enemies/Brick2", 8, 3, 20),
+            new ObjectGamePosition("enemies/Brick2", 11, 3, 20),
+
+            new ObjectGamePosition("enemies/Brick3", 2, 4, 20), 
+            new ObjectGamePosition("enemies/Brick3", 3, 4, 20), 
+            new ObjectGamePosition("enemies/Brick3", 4, 4, 20), 
+            new ObjectGamePosition("enemies/Brick3", 7, 4, 20), 
+            new ObjectGamePosition("enemies/Brick3", 8, 4, 10), 
+            new ObjectGamePosition("enemies/Brick3", 9, 4, 10),
+            
+            new ObjectGamePosition("enemies/Brick3", 1, 5, 10),
+            new ObjectGamePosition("enemies/Brick3", 2, 5, 10), 
+            new ObjectGamePosition("enemies/Brick3", 3, 5, 10), 
+            new ObjectGamePosition("enemies/Brick3", 4, 5, 10), 
+            new ObjectGamePosition("enemies/Brick3", 7, 5, 10),
+            new ObjectGamePosition("enemies/Brick3", 8, 5, 10), 
+            new ObjectGamePosition("enemies/Brick3", 9, 5, 10), 
+            new ObjectGamePosition("enemies/Brick3", 10, 5, 10), 
+            
+            new ObjectGamePosition("enemies/Brick2", 1, 6, 30),
+            new ObjectGamePosition("enemies/Brick2", 3, 6, 10),
+            new ObjectGamePosition("enemies/Brick2", 5, 6, 10),
+            new ObjectGamePosition("enemies/Brick2", 6, 6, 30),
+            new ObjectGamePosition("enemies/Brick2", 8, 6, 30),
+            new ObjectGamePosition("enemies/Brick2", 10, 6, 10),
+
+            new ObjectGamePosition("extras/Magic Ball Particle", 5, 2, 1),
+            
+            new ObjectGamePosition("extras/Score Ball Particle", 6, 3, 1),
+            new ObjectGamePosition("extras/Score Ball Particle", 5, 4, 1),
+            new ObjectGamePosition("extras/Score Ball Particle", 6, 5, 1),
+        };
+    }
+}
