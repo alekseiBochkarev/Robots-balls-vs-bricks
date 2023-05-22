69b431eadeb521b21b24956931f2de62cb3965d7
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2023-05-22 13:49:37 +0700

    add scene with boss

---
+++ Assets/Scripts/Placing/SceneConfiguration6.cs
@@ -0,0 +1,18 @@
+﻿public class SceneConfiguration6 : SceneConfiguration
+{
+    void Awake()
+    {
+        _objectGamePositions = new[]
+        {
+            new ObjectGamePosition("enemies/Brick3", 1, 2, 20),
+            new ObjectGamePosition("enemies/Brick3", 10, 2, 20),
+            
+            new ObjectGamePosition("enemies/BrickBoss", 5, 4, 200),
+
+            new ObjectGamePosition("extras/Magic Ball Particle", 5, 2, 1),
+            
+            new ObjectGamePosition("extras/Score Ball Particle", 3, 3, 1),
+            new ObjectGamePosition("extras/Score Ball Particle", 7, 3, 1),
+        };
+    }
+}
