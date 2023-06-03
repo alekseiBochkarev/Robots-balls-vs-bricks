cb47aab81f1ccf6c651bd8484c88521015042cf6
alekseiBochkarev <aleksei.bochkarev@brandmaker.com>
2023-06-03 18:01:38 +0700

    add sound for bomb

--- Assets/Scripts/Gameplay/balls/BombBall/BombAttack.cs
+++ Assets/Scripts/Gameplay/balls/BombBall/BombAttack.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class BombAttack : MonoBehaviour, AttackBehaviour
 {
     GameObject ballPrefab;
+    AudioSource audioSource; 
 
     public void SpecialAttack(Vector3 position, GameObject brick)
     {
        ballPrefab = Resources.Load<GameObject>("BombCloneBall");
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
        GameObject bombCloneBall = Instantiate(ballPrefab, position, Quaternion.identity);
     }
 }
