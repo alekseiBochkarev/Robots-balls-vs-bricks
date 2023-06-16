b41b4d55cfe7c83cb7109e0ae756cc3c7d1b747d
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2023-06-16 17:26:48 +0700

    add buttons for beforeStartPanel

---
+++ Assets/Scripts/UI/BeforeStartPanelController.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class BeforeStartPanelController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _shopButtonSmall;
+        [SerializeField] private GameObject _shopButtonBig;
+        
+    }
+}
