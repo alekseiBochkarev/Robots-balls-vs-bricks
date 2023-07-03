a3df3cb2429f9e1f7066a0da405659343cf8e76d
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2023-07-03 19:44:15 +0700

    change skins panel

--- Assets/Scripts/UI/SkinButton.cs
+++ Assets/Scripts/UI/SkinButton.cs
@@ -2,26 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SkinButton : MonoBehaviour
 {
-    private Image _buttonImage;
+    [SerializeField] private string _robotName;
+    [SerializeField] private Image _buttonImage;
+	[SerializeField] private GameObject _textMeshPro;
     private GameObject[] _heroSkins;
     private GameObject _skinMenuHeroImage;
     void OnEnable()
     {
+		_buttonImage.sprite = Resources.Load<Sprite>("Robots/" + _robotName);
+		_textMeshPro.GetComponent<TMP_Text>().text = _robotName;
         _heroSkins = GameObject.FindGameObjectsWithTag("HeroSkin");
         _skinMenuHeroImage = GameObject.FindWithTag("SkinMenuHeroImage");
-        _buttonImage = GetComponent<Image>();
     }
 
     public void changeCurrentSkin()
     {
         foreach (var _heroSkin in _heroSkins)
         {
-            _heroSkin.GetComponent<SpriteRenderer>().sprite = _buttonImage.sprite;
+            _heroSkin.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Robots/" + _robotName);
         }
 
-        _skinMenuHeroImage.GetComponent<Image>().sprite = _buttonImage.sprite;
+        _skinMenuHeroImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Robots/" + _robotName);
     }
 }
