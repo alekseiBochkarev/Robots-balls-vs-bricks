5f062fe14b54b0189deea5ad7ffd0af926a9bfd3
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2023-07-04 15:23:11 +0700

    fix skin panel and add lock

--- Assets/Scripts/UI/SkinButton.cs
+++ Assets/Scripts/UI/SkinButton.cs
@@ -9,6 +9,8 @@ public class SkinButton : MonoBehaviour
     [SerializeField] private string _robotName;
     [SerializeField] private Image _buttonImage;
 	[SerializeField] private GameObject _textMeshPro;
+	[SerializeField] private int _minLevelWhereAvailable;
+	[SerializeField] private GameObject _lockImage;
     private GameObject[] _heroSkins;
     private GameObject _skinMenuHeroImage;
     void OnEnable()
@@ -17,6 +19,18 @@ public class SkinButton : MonoBehaviour
 		_textMeshPro.GetComponent<TMP_Text>().text = _robotName;
         _heroSkins = GameObject.FindGameObjectsWithTag("HeroSkin");
         _skinMenuHeroImage = GameObject.FindWithTag("SkinMenuHeroImage");
+		//lock button if not available
+		if (_minLevelWhereAvailable >= SaveManager.LoadSceneData()) 
+		{
+			_lockImage.SetActive(true);
+			_buttonImage.color = new Color32(0,0,0,100);
+			GetComponent<Button>().interactable = false;
+        } else 
+		{
+			_lockImage.SetActive(false);
+			_buttonImage.color = new Color32(255,255,225,100);
+			GetComponent<Button>().interactable = true;
+		}
     }
 
     public void changeCurrentSkin()
