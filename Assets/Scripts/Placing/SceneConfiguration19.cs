76d5dd57efcd57400bb2d7c4c1bfd8c637f2ded5
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2023-07-20 18:25:00 +0700

    add 19 and 20 levels

---
+++ Assets/Scripts/Placing/SceneConfiguration19.cs
@@ -0,0 +1,61 @@
+﻿public class SceneConfiguration19 : SceneConfiguration
+{
+    void Awake()
+    {
+        _objectGamePositions = new[]
+        {
+            new ObjectGamePosition("enemies/BrickSquarePurple", 0, 1, 12),
+            new ObjectGamePosition("enemies/BrickAborigen", 1, 1, 12), 
+            new ObjectGamePosition("enemies/BrickSquarePurple", 2, 1, 12),
+            new ObjectGamePosition("enemies/BrickSimpleTriangle3", 9, 1, 12), 
+            new ObjectGamePosition("enemies/BrickSquarePurple", 10, 1, 12),
+            
+            new ObjectGamePosition("enemies/BrickSimpleTriangle3", 0, 2, 12), 
+            new ObjectGamePosition("enemies/BrickSquarePurple", 1, 2, 12),
+            new ObjectGamePosition("enemies/BrickSimpleTriangle4", 2, 2, 12),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 4, 2, 12),
+            new ObjectGamePosition("enemies/BrickAborigen", 5, 2, 12),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 6, 2, 12),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 7, 2, 12),
+            new ObjectGamePosition("enemies/BrickSimpleTriangle1", 8, 2, 12),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 11, 2, 12),
+
+            new ObjectGamePosition("enemies/BrickSimpleTriangle3", 4, 3, 12), 
+            new ObjectGamePosition("enemies/BrickSquarePurple", 5, 3, 12),
+            new ObjectGamePosition("enemies/BrickAborigen", 6, 3, 12), 
+            new ObjectGamePosition("enemies/BrickSquarePurple", 7, 3, 12),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 9, 3, 12),
+            new ObjectGamePosition("enemies/BrickSimpleTriangle3", 11, 3, 12),
+
+            new ObjectGamePosition("enemies/BrickSquarePurple", 0, 4, 12), 
+            new ObjectGamePosition("enemies/BrickAborigen", 1, 4, 12),
+            new ObjectGamePosition("enemies/BrickSimpleTriangle1", 2, 4, 12),
+            new ObjectGamePosition("enemies/BrickSimpleTriangle3", 5, 4, 12),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 6, 4, 12),
+            new ObjectGamePosition("enemies/BrickAborigen", 7, 4, 12),
+            new ObjectGamePosition("enemies/BrickAborigen", 8, 4, 12),
+            new ObjectGamePosition("enemies/BrickSimpleTriangle1", 9, 4, 12),
+
+            new ObjectGamePosition("enemies/BrickSquarePurple", 0, 5, 12),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 1, 5, 12),
+            new ObjectGamePosition("enemies/BrickAborigen", 2, 5, 12),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 3, 5, 12),
+            new ObjectGamePosition("enemies/BrickAborigen", 8, 5, 12),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 9, 5, 12),
+            
+            
+            new ObjectGamePosition("enemies/BrickSquarePurple", 4, 6, 12),
+            new ObjectGamePosition("enemies/BrickSimpleTriangle4", 5, 6, 12),
+            new ObjectGamePosition("enemies/BrickSimpleTriangle2", 7, 6, 12),
+            
+            new ObjectGamePosition("extras/Magic Ball Particle", 3, 1, 1), 
+            new ObjectGamePosition("extras/Magic Ball Particle", 11, 4, 1), 
+            new ObjectGamePosition("extras/Magic Ball Particle", 0, 6, 1),
+            
+            new ObjectGamePosition("extras/Score Ball Particle", 9, 2, 1), 
+            new ObjectGamePosition("extras/Score Ball Particle", 0, 3, 1), 
+            new ObjectGamePosition("extras/Score Ball Particle", 4, 5, 1),
+            
+        };
+    }
+}
