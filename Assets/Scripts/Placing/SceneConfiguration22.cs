59ae8261b1a12e015ee8ef6e5a6c515c9252ecf7
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2023-09-08 06:52:16 +0700

    add scenes 22 and 23 and robot r14

---
+++ Assets/Scripts/Placing/SceneConfiguration22.cs
@@ -0,0 +1,63 @@
+﻿public class SceneConfiguration22 : SceneConfiguration
+{
+    void Awake()
+    {
+        _objectGamePositions = new[]
+        {
+            new ObjectGamePosition("enemies/UFO", 5, 5, 1),
+
+            new ObjectGamePosition("enemies/BrickFire", 2, 2, 8),
+            new ObjectGamePosition("enemies/BrickSkeleton", 3, 2, 14),
+            new ObjectGamePosition("enemies/BrickSkeleton", 4, 2, 14),
+            new ObjectGamePosition("enemies/BrickSkeleton", 5, 2, 14),
+            new ObjectGamePosition("enemies/BrickSkeleton", 6, 2, 14),
+            new ObjectGamePosition("enemies/BrickSkeleton", 7, 2, 14),
+            new ObjectGamePosition("enemies/BrickSkeleton", 8, 2, 14),
+            new ObjectGamePosition("enemies/BrickFire", 9, 2, 8),
+            
+            new ObjectGamePosition("enemies/BrickFire", 1, 3, 8),
+            new ObjectGamePosition("enemies/BrickSkeleton", 2, 3, 14), 
+            new ObjectGamePosition("enemies/BrickSkeleton", 3, 3, 14), 
+            new ObjectGamePosition("enemies/BrickSkeleton", 4, 3, 14), 
+            new ObjectGamePosition("enemies/BrickBombaSmall", 5, 3, 8),
+            new ObjectGamePosition("enemies/BrickBombaSmall", 6, 3, 8), 
+            new ObjectGamePosition("enemies/BrickSkeleton", 7, 3, 14), 
+            new ObjectGamePosition("enemies/BrickSkeleton", 8, 3, 14),
+            new ObjectGamePosition("enemies/BrickSkeleton", 9, 3, 14),
+            new ObjectGamePosition("enemies/BrickFire", 10, 3, 8),
+            
+            new ObjectGamePosition("enemies/BrickFire", 0, 4, 8),
+            new ObjectGamePosition("enemies/BrickSquare", 1, 4, 14), 
+            new ObjectGamePosition("enemies/BrickSquare", 10, 4, 14), 
+            new ObjectGamePosition("enemies/BrickFire", 11, 4, 8), 
+           
+            new ObjectGamePosition("enemies/BrickSquarePurple", 1, 5, 8),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 2, 5, 8), 
+            new ObjectGamePosition("enemies/BrickSquarePurple", 3, 5, 10), 
+            new ObjectGamePosition("enemies/BrickSquarePurple", 7, 5, 8),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 8, 5, 8),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 9, 5, 8),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 10, 5, 8),
+
+            new ObjectGamePosition("enemies/BrickSquarePurple", 1, 6, 10),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 2, 6, 10),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 3, 6, 10),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 6, 6, 8), 
+            new ObjectGamePosition("enemies/BrickSquarePurple", 7, 6, 10), 
+            new ObjectGamePosition("enemies/BrickSquarePurple", 8, 6, 8),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 9, 6, 8),
+            new ObjectGamePosition("enemies/BrickSquarePurple", 10, 6, 8),
+
+            new ObjectGamePosition("enemies/BrickFire", 6, 7, 8),
+            new ObjectGamePosition("enemies/BrickFire", 7, 7, 8),
+
+            new ObjectGamePosition("extras/Magic Ball Particle", 7, 4, 1),
+            new ObjectGamePosition("extras/Magic Ball Particle", 6, 5, 1),
+            
+            new ObjectGamePosition("extras/Score Ball Particle", 10, 2, 1),
+            new ObjectGamePosition("extras/Score Ball Particle", 11, 3, 1),
+            new ObjectGamePosition("extras/Score Ball Particle", 5, 6, 1),
+            new ObjectGamePosition("extras/Score Ball Particle", 5, 7, 1),
+        };
+    }
+}
---
+++ Assets/Scripts/Placing/SceneConfiguration23.cs
@@ -0,0 +1,54 @@
+﻿public class SceneConfiguration23 : SceneConfiguration
+{
+    void Awake()
+    {
+        _objectGamePositions = new[]
+        {
+            new ObjectGamePosition("enemies/UFO", 5, 5, 4), 
+            
+            new ObjectGamePosition("enemies/BrickPrizSkin8", 5,5,4),
+            
+            new ObjectGamePosition("enemies/BrickBombaSmall", 3, 2, 4), 
+            new ObjectGamePosition("enemies/BrickSquare", 4, 2, 4),
+            new ObjectGamePosition("enemies/BrickSquare", 5, 2, 4),
+            new ObjectGamePosition("enemies/BrickSquare", 6, 2, 4),
+            new ObjectGamePosition("enemies/BrickSquare", 7, 2, 4),
+            
+            new ObjectGamePosition("enemies/BrickSquare", 3, 3, 4), 
+            new ObjectGamePosition("enemies/BrickSquare", 4, 3, 4), 
+            new ObjectGamePosition("enemies/BrickBombaSmall", 5, 3, 4), 
+            new ObjectGamePosition("enemies/BrickSquare", 6, 3, 4), 
+            new ObjectGamePosition("enemies/BrickSquare", 7, 3, 4), 
+            
+            new ObjectGamePosition("enemies/BrickSquare", 3, 4, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 4, 4, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 5, 4, 4), 
+            new ObjectGamePosition("enemies/BrickSquare", 6, 4, 4), 
+            new ObjectGamePosition("enemies/BrickBombaSmall", 7, 4, 4), 
+            
+            new ObjectGamePosition("enemies/BrickSquareBlue", 3, 5, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 4, 5, 4), 
+            new ObjectGamePosition("enemies/BrickSquare", 6, 5, 4), 
+            new ObjectGamePosition("enemies/BrickSquare", 7, 5, 4), 
+            
+            new ObjectGamePosition("enemies/BrickBombaSmall", 3, 6, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 4, 6, 4), 
+            new ObjectGamePosition("enemies/BrickSquare", 5, 6, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 6, 6, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 7, 6, 4), 
+            
+            new ObjectGamePosition("enemies/BrickSquareBlue", 3, 7, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 4, 7, 4), 
+            new ObjectGamePosition("enemies/BrickBombaSmall", 5, 7, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 6, 7, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 7, 7, 4), 
+            
+            new ObjectGamePosition("enemies/BrickSquareBlue", 3, 8, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 4, 8, 4), 
+            new ObjectGamePosition("enemies/BrickSquare", 5, 8, 4), 
+            new ObjectGamePosition("enemies/BrickBombaSmall", 6, 8, 4), 
+            new ObjectGamePosition("enemies/BrickSquareBlue", 7, 8, 4), 
+			
+        };
+    }
+}
