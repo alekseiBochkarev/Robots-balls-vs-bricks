ff5cffc184cf71fe0f98cfeb9ad7dc9212102e05
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2024-01-25 14:43:12 +0700

    add button and script for rewarded ads

---
+++ Assets/Scripts/RewardAdsManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+using Assets.Scripts.Data_Managing;
+using Assets.Scripts.DataManaging.Utills;
+
+public class RewardAdsManager : MonoBehaviour
+{
+    public YandexGame sdk;
+
+    public GameObject adsButton;
+
+    void Start()
+    {
+        adsButton.SetActive(true);
+    }
+
+    public void ShowRewAdd()
+    {
+        sdk._RewardedShow(1);
+    }
+
+    public void AddCoinsAfterAdd()
+    {
+        WalletController.Instance.AddMoneyAndShow(1000);
+        adsButton.SetActive(false);
+    }
+}
