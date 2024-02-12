14c471e1b2671c72775e8934bb7d3c2677a92029
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2024-02-12 15:39:08 +0700

    change sceneConfiguration

--- Assets/Scripts/Placing/SceneConfiguration.cs
+++ Assets/Scripts/Placing/SceneConfiguration.cs
@@ -2,5 +2,15 @@
 public class SceneConfiguration : MonoBehaviour
 {
     public ObjectGamePosition[] _objectGamePositions;
-    
+    public ObjectGamePosition[] SetObjects() {
+        return new ObjectGamePosition[1];
+    }
+
+    public void Awake()
+    {
+        Debug.Log("Awake for scene Configuration");
+        SceneConfiguration1 sceneConfiguration = new SceneConfiguration1();
+        _objectGamePositions = sceneConfiguration.SetObjects();
+    }
+
 }
--- Assets/Scripts/Placing/SceneConfiguration1.cs
+++ Assets/Scripts/Placing/SceneConfiguration1.cs
@@ -1,14 +1,14 @@
 ﻿public class SceneConfiguration1 : SceneConfiguration
 {
-    void Awake()
+   public ObjectGamePosition[] SetObjects()
     {
         _objectGamePositions = new[]
         {
-            new ObjectGamePosition("enemies/UFO", 5, 5, 1), 
-            
-            new ObjectGamePosition("enemies/BrickSkeleton", 1, 3, 2), 
-			new ObjectGamePosition("enemies/BrickSkeleton", 2, 3, 2),
-			new ObjectGamePosition("enemies/BrickSquare", 3, 3, 2),
+            new ObjectGamePosition("enemies/UFO", 5, 5, 1),
+
+            new ObjectGamePosition("enemies/BrickSkeleton", 1, 3, 2),
+            new ObjectGamePosition("enemies/BrickSkeleton", 2, 3, 2),
+            new ObjectGamePosition("enemies/BrickSquare", 3, 3, 2),
             new ObjectGamePosition("enemies/BrickSquare", 4, 3, 2),
             new ObjectGamePosition("enemies/BrickSquare", 5, 3, 2),
             new ObjectGamePosition("enemies/BrickSquare", 6, 3, 2),
@@ -16,9 +16,9 @@
             new ObjectGamePosition("enemies/BrickSquare", 8, 3, 2),
             new ObjectGamePosition("enemies/BrickSkeleton", 9, 3, 2),
             new ObjectGamePosition("enemies/BrickSkeleton", 10, 3, 2),
-            
+
             new ObjectGamePosition("enemies/BrickSkeleton", 10, 4, 2),
-           
+
             new ObjectGamePosition("enemies/BrickSquare", 1, 5, 2),
             new ObjectGamePosition("enemies/BrickSquare", 3, 5, 2),
             new ObjectGamePosition("enemies/BrickSquare", 4, 5, 2),
@@ -27,12 +27,12 @@
             new ObjectGamePosition("enemies/BrickSquare", 7, 5, 2),
             new ObjectGamePosition("enemies/BrickSquare", 8, 5, 2),
             new ObjectGamePosition("enemies/BrickSquare", 10, 5, 2),
-            
+
             new ObjectGamePosition("enemies/BrickSkeleton", 1, 6, 2),
-            
-            new ObjectGamePosition("enemies/BrickSkeleton", 1, 7, 2), 
-			new ObjectGamePosition("enemies/BrickSkeleton", 2, 7, 2),
-			new ObjectGamePosition("enemies/BrickSkeleton", 3, 7, 2),
+
+            new ObjectGamePosition("enemies/BrickSkeleton", 1, 7, 2),
+            new ObjectGamePosition("enemies/BrickSkeleton", 2, 7, 2),
+            new ObjectGamePosition("enemies/BrickSkeleton", 3, 7, 2),
             new ObjectGamePosition("enemies/BrickSquare", 4, 7, 2),
             new ObjectGamePosition("enemies/BrickSquare", 5, 7, 2),
             new ObjectGamePosition("enemies/BrickSquare", 6, 7, 2),
@@ -40,9 +40,12 @@
             new ObjectGamePosition("enemies/BrickSquare", 8, 7, 2),
             new ObjectGamePosition("enemies/BrickSquare", 9, 7, 2),
             new ObjectGamePosition("enemies/BrickSkeleton", 10, 7, 2),
-            
+
             new ObjectGamePosition("extras/Magic Ball Particle", 1, 2, 1),
-            new ObjectGamePosition("extras/Score Ball Particle", 10, 2, 1)
+            new ObjectGamePosition("extras/Score Ball Particle", 10, 2, 1),
         };
+        return _objectGamePositions;
     }
+    
+    
 }
