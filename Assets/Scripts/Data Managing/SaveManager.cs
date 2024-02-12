af6d9efcbf09b5ac3a5fb6ec8c44add669f8f54d
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2024-02-12 16:51:00 +0700

    add some changexs for SaveManager

--- Assets/Scripts/Data Managing/SaveManager.cs
+++ Assets/Scripts/Data Managing/SaveManager.cs
@@ -21,4 +21,24 @@ public class SaveManager : MonoBehaviour
             return defaultScene;
         }
     }
+
+    public static void SaveDay(int day)
+    {
+        PlayerPrefs.SetInt("CurrentDay", day);
+        PlayerPrefs.Save();
+    }
+
+    public static int LoadDayData()
+    {
+        int defaultDay = 1;
+        if (PlayerPrefs.HasKey("CurrentDay"))
+        {
+            return PlayerPrefs.GetInt("CurrentDay");
+        }
+        else
+        {
+            //Debug.Log("That's OK. Just There is no save data!"); 
+            return defaultDay;
+        }
+    }
 }
