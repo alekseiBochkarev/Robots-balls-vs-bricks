6441bf22cd23ea1cd477fe1aca961c535437db52
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2024-06-24 10:04:05 +0700

    change playButton script

--- Assets/Scripts/UI/PlayButton.cs
+++ Assets/Scripts/UI/PlayButton.cs
@@ -7,9 +7,27 @@ public class PlayButton : MonoBehaviour
 {
     [SerializeField] private GameObject _textMeshPro;
 
+    private void Awake()
+    {
+        EventManager.OnLanguageChanged += Translate;
+    }
+
     void OnEnable()
     {
         // _textMeshPro.GetComponent<TMP_Text>().text = Translator.Translate("LEVEL ") + SceneManager.GetActiveScene().buildIndex;
+        Translate();
+    }
+
+    void Translate()
+    {
         _textMeshPro.GetComponent<TMP_Text>().text = Translator.Translate("DAY ") + SaveManager.LoadDayData();
     }
+
+    private void OnDestroy()
+    {
+        // EventManager.UpgradeStats -= ShowBallsAmountOnHUD;
+        EventManager.OnLanguageChanged -= Translate;
+    }
+
+
 }
